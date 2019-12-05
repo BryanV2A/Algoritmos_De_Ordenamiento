066794c940df57f66de678ed669ad7e1092473b4
Bryan Alonso Valencia <alonsoval2.03@gmail.com>
2019-12-04 23:01:12 -0600

    feat: Se agrego el método para la solucion del primer problema.

--- EliminarElementosRepetidos.py
+++ EliminarElementosRepetidos.py
@@ -1,1 +1,16 @@
 class Arreglos:
+
+    def EliminarDuplicados(self, Arreglo):
+
+        Arreglo_Ordenado = []
+
+        for elemento in Arreglo:
+            if not elemento in Arreglo_Ordenado:
+                Arreglo_Ordenado.append(elemento)
+
+        return Arreglo_Ordenado
+
+Arreglo = [4, 7, 11, 4, 9, 5, 11, 7, 3, 5]
+Ordenar = Arreglos()
+Resultado = Ordenar.EliminarDuplicados(Arreglo)
+print(Resultado)
