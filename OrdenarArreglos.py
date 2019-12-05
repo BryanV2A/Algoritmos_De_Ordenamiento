d4c3b11d8c1592d87bbf20a8b7875cd134726653
Bryan Alonso Valencia <alonsoval2.03@gmail.com>
2019-12-04 23:14:16 -0600

    feat: Proyecto inicial para la resolucion del problema 2.
    feat: Se agregaron la clase y métodos del método de ordenamiento QuickSort.

---
+++ OrdenarArreglos.py
@@ -0,0 +1,27 @@
+class QuickSort:
+    A = []
+    B = []
+    C = []
+
+    def intercambia(self, a, x, y):
+        tmp = a[x]
+        a[x] = a[y]
+        a[y] = tmp
+
+    def Particionar(self, a, p, r):
+        x = a[r]
+        i = p - 1
+        for j in range(p, r):
+            if (a[j] <= x):
+                i = i + 1
+                self.intercambia (a, i, j)
+        self.intercambia(a, i+1, r)
+        return i + 1
+
+    def QuickSort(self, a, p, r):
+        if (p < r):
+            q = self.Particionar(a, p, r)
+            #print (A[p:r])
+            self.QuickSort(a, p, q-1)
+            self.QuickSort(a, q+1, r)
+        return a
